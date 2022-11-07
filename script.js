ac9898a0845c0ad4eb8dd1698269c0e93724075f
“natios” <natios100@gmail.com>
2022-11-07 19:59:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+            reveals[i].classList.remove("disactivate");
+        } 
+        else if (elementTop == 0) {
+            reveals[i].classList.remove("active");
+            reveals[i].classList.remove("disactivate");
+        }
+        else {
+            reveals[i].classList.remove("active");
+            reveals[i].classList.add("disactivate");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+function playAudio(path) {
+    let audio = new Audio(path);
+    audio.play();
+}
